2f3863328205b355f543a2c286464e8c300d6ac2
CreateSource <abnormalhare822@gmail.com>
2023-11-22 11:55:14 -0500

    first commit

---
+++ src/REL/subchrselDll/subchrselDll.h
@@ -0,0 +1,11 @@
+#include "common.h"
+#include "REL/executor.h"
+
+void fn_8000C760(void*, s32, s32, s32, s32);
+s32 fn_8000CA3C();
+void fn_800414AC(s16, s16, s16);
+extern void fn_1_164(void);
+
+extern u8 lbl_1_bss_0;
+extern u8 lbl_801D3AAC;
+extern u16 lbl_801D3AD0;
