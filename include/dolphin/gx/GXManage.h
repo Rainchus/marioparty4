2db537cf19f0aaf448a227f287183d1371cc7138
gamemasterplc <gamemasterplc@gmail.com>
2023-12-27 21:34:30 -0600

    Fix perf GX functions

--- include/dolphin/gx/GXManage.h
+++ include/dolphin/gx/GXManage.h
@@ -8,9 +8,13 @@ extern "C" {
 #endif
 
 typedef void (*GXDrawDoneCallback)(void);
+typedef void (*GXDrawSyncCallback)(u16 token);
 
 GXFifoObj* GXInit(void* base, u32 size);
 GXDrawDoneCallback GXSetDrawDoneCallback(GXDrawDoneCallback cb);
+void GXSetDrawSync(u16 token);
+GXDrawSyncCallback GXSetDrawSyncCallback(GXDrawSyncCallback callback);
+
 void GXDrawDone(void);
 void GXSetDrawDone(void);
 void GXFlush(void);
