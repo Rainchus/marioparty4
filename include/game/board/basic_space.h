4a6228b7058d27d562a7bac24886f6744106deeb
CreateSource <abnormalhare822@gmail.com>
2023-12-31 23:46:00 -0500

    basic_space.c complete, names incoming

---
+++ include/game/board/basic_space.h
@@ -0,0 +1,54 @@
+#include "common.h"
+#include "math.h"
+#include "game/process.h"
+#include "game/audio.h"
+#include "game/object.h"
+
+extern Process *boardObjMan;
+
+typedef struct bit_copy {
+    struct {
+        u8 hide : 1;
+        u8 field08_bit1 : 1;
+        u8 field08_bit2 : 1;
+        u8 field08_bit3 : 3;
+        u8 field08_bit6 : 1;
+        u8 field08_bit7 : 1;
+    };
+    s8 unk_01;
+    s8 unk_02;
+    s8 unk_03;
+    u16 unk_04;
+    u16 unk_06;
+    s16 unk_08;
+    s16 unk_0A;
+    s16 unk_0C;
+    s16 unk_0E;
+} bitcopy;
+
+omObjData *lbl_8013A1F0[4] = {
+    (omObjData *)NULL,
+    (omObjData *)NULL,
+    (omObjData *)NULL,
+    (omObjData *)NULL
+};
+
+s32 lbl_8013A200[10] = {
+    0x7000C, 0x7000D,
+    0x7000E, 0x7000F,
+    0x70010, 0x70011,
+    0x70012, 0x70013,
+    0x70014, 0x70015
+};
+
+void BoardEventLandBlue(s32);
+void BoardEventLandRed(s32);
+s8 fn_8007FE70(Point3d*, s8);
+s32 fn_80080058(s32);
+void fn_800800C4(s32);
+void fn_80080124(bitcopy*, Vec*);
+void fn_80080360(omObjData*);
+void fn_800804F8(omObjData*, bitcopy*);
+void fn_800806B0(omObjData*, bitcopy*);
+void fn_8008094C(omObjData*, bitcopy*);
+void fn_80080AF4(omObjData*, bitcopy*);
