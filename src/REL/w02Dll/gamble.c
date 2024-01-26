2fcad7161dde4411d7d3ca9c14dc72eeec0011a9
M0liusX <marioamolina1996@gmail.com>
2024-01-26 13:02:16 -0600

    decomp w02dll fn_1_394C

--- src/REL/w02Dll/gamble.c
+++ src/REL/w02Dll/gamble.c
@@ -1,8 +1,28 @@
 #include "dolphin.h"
+#include "game/process.h"
+#include "game/gamework_data.h"
+#include "game/board/main.h"
+
+Process* lbl_1_bss_50;
+s32 lbl_1_bss_54;
 
 
-s32 lbl_1_bss_50;
+void fn_1_2D04();
+
 
 void fn_1_3938(void) {
-    lbl_1_bss_50 = 0;
+    lbl_1_bss_50 = NULL;
+}
+
+
+void fn_1_394C(s32 arg0) {
+    s32 currPlayer;
+    currPlayer = GWSystem.player_curr;
+
+    lbl_1_bss_54 = arg0;
+    lbl_1_bss_50 = HuPrcChildCreate(fn_1_2D04, 0x2003U, 0x2000U, 0, boardMainProc);
+    HuPrcDestructorSet2(lbl_1_bss_50, fn_1_3938);
+    while (lbl_1_bss_50) {
+        HuPrcVSleep();
+    }
 }
