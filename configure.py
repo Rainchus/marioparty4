d06c30c46e32816ce45fc0dbe84b13d2ba5b57e8
gamemasterplc <gamemasterplc@gmail.com>
2024-06-11 13:12:18 -0500

    Finish w02dll

--- configure.py
+++ configure.py
@@ -1488,7 +1488,7 @@ config.libs = [
             Object(Matching, "REL/w02Dll/gamble.c"),
             Object(Matching, "REL/w02Dll/mg_coin.c"),
             Object(Matching, "REL/w02Dll/mg_item.c"),
-            Object(NonMatching, "REL/w02Dll/shuffleboard.c"),
+            Object(Matching, "REL/w02Dll/shuffleboard.c"),
             Object(Matching, "REL/w02Dll/roulette.c"),
         },
     ),
