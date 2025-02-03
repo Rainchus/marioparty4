a144a790f1b32eda6a2a8c28ced32cd2906a0303
CreateSource <abnormalhare822@gmail.com>
2025-02-02 20:44:39 -0500

    map progress

--- include/REL/m427Dll.h
+++ include/REL/m427Dll.h
@@ -5,6 +5,7 @@ void fn_1_32F4(Process*);
 void fn_1_3A88(void);
 void fn_1_E98C(Process*);
 void fn_1_EC54(void);
+s32 fn_1_10FE8(s16, s32*);
 void fn_1_110E8(s32, s32*);
 void fn_1_111C0(s32, s32*);
 void fn_1_11298(f32);
